a65468276f5849d53e3a625c5c737ab4c955629e
David Pak <sungh1404@gmail.com>
2023-10-15 01:33:36 -0700

    Almost finished loading-page, just need to change text when animation ends

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 // Get the "Add Another" button and rectangle container
 const addAnotherButton = document.getElementById("add-another");
 const rectangleContainer = document.querySelector(".div");
+const progressBar = document.querySelector('.loading-rectangle');
+const progressText = document.getElementById('progressText');
+const completionText = document.getElementById('completionText');
+const brainProgressText = document.getElementById('brainCollect');
+const brainLotsText = document.getElementById('brainLots');
+
 
 // Listen for the "Add Another" button click
 addAnotherButton.addEventListener("click", function() {
@@ -10,4 +16,13 @@ addAnotherButton.addEventListener("click", function() {
   newInput.className = "rectangle";
   newInput.placeholder = "Write Your Feelings Here";
   rectangleContainer.appendChild(newInput);
+});
+
+progressBar.addEventListener('animationend', () => {
+  // Update text when the animation iteration is complete
+  alert("bruh");
+  progressText.style.display = 'none'; // Hide progress text
+  completionText.style.visibility = 'visible'; // Show completion text
+  brainProgressText.style.display = 'none';
+  brainLotsText.style.display = 'block'; // Show completion text for "Lots of Brainwaves"
 });
