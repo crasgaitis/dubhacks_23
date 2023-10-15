827071fd3bd4d071d1259be43f1240f600b234bb
crasgaitis <crasgaitis@gmail.com>
2023-10-15 01:25:39 -0700

    flask flow

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/select-genre-page.html')
+def select_genre_page():
+    return render_template('select-genre-page.html')
+
+@app.route('/user-input-feelings-page.html')
+def user_input_feelings_page():
+    return render_template('user-input-feelings-page.html')
+
+@app.route('/egg-page.html')
+def egg_page():
+    return render_template('egg-page.html')
+
+@app.route('/results.html')
+def results_page():
+    return render_template('results.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
